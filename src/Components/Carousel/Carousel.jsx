42981a8b5046f9d97007fb269333689b3803875c
nicole <nicolerodriguezcab@gmail.com>
2024-05-02 17:16:35 -0400

    added img overlay  to carousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -9,32 +9,13 @@ function CarouselComponent({ backendEvents }) {
 		setIndex(selectedIndex);
 	};
 
-	const data = [
-		{
-			src: "https://images.unsplash.com/photo-1502657877623-f66bf489d236",
-			title: "March to DC: Rally for Change - Join Sarah's Movement!",
-		},
-		{
-			src: "https://images.unsplash.com/photo-1527549993586-dff825b37782",
-			title: "Community Cleanup: Let's Make Our Neighborhood Shine ",
-		},
-		{
-			src: "https://images.unsplash.com/photo-1502657877623-f66bf489d236",
-			title: "Global Solidarity Summit: Uniting for a Fairer, More Just World!",
-		},
-	];
-
 	return (
-		<div className="" style={{ padding: "40px" }}>
+		<div className="" style={{ padding: "30px" }}>
 			<div className="fw-bold fs-4 my-3 d-flex justify-content-center">
 				Most popular events
 			</div>
 			<div style={{ padding: "10px" }}>
-				<Carousel
-					activeIndex={index}
-					onSelect={handleSelect}
-					indicators={false}
-				>
+				<Carousel activeIndex={index} onSelect={handleSelect} indicators={true}>
 					{backendEvents.map((event, index) => (
 						<Carousel.Item key={index}>
 							<img
@@ -43,6 +24,17 @@ function CarouselComponent({ backendEvents }) {
 								src={event.event_photo}
 								alt={event.event_title}
 							/>
+							<div
+								style={{
+									position: "absolute",
+									top: 0,
+									left: 0,
+									width: "100%",
+									height: "100%",
+									backgroundColor: "rgba(0, 0, 0, 0.5)",
+									borderRadius: "20px",
+								}}
+							></div>
 							<Carousel.Caption>
 								<h3>{event.event_title}</h3>
 							</Carousel.Caption>
