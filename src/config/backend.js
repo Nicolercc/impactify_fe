da1ab4c9f1291e7f153768b026a848d5814ad0b0
nicole <nicolerodriguezcab@gmail.com>
2026-02-04 19:28:46 -0500

    Enhance user experience by improving loading indicators and optimizing UI responsiveness

---
+++ src/config/backend.js
@@ -0,0 +1,11 @@
+/**
+ * Centralized Backend URL Configuration
+ * 
+ * To change the backend URL, simply update the value below.
+ * This will be used throughout the entire application.
+ */
+
+const BACKEND_URL = "http://localhost:4000";
+
+export default BACKEND_URL;
+
